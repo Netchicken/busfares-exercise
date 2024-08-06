2566e30172613dc9d657e7b2628abf2f5811a96d
Gary Dix <intelproof@gmail.com>
2024-08-06 14:12:33 +1200

    final

--- busfares exercise/Form1.cs
+++ busfares exercise/Form1.cs
@@ -6,11 +6,6 @@ namespace busfares_exercise
     public partial class Form1 : Form
     {
 
-
-
-
-
-
         public Form1()
         {
             InitializeComponent();
@@ -18,17 +13,19 @@ namespace busfares_exercise
 
         private void btncalculate_Click(object sender, EventArgs e)
         {
+            //Creae your variables and pass data to them
             Single percentDisc, ash, dun = 0;
             string discountText = "";
+
             string name = txtName.Text;
             int age = Convert.ToInt16(txtAge.Text);
             ash = Convert.ToSingle(txtAshFare.Text);
             dun = Convert.ToSingle(txtDunFare.Text);
+
+            //gives 20% discount
             percentDisc = 0.2f;
 
-
-
-            if ((cbxDisabled.Checked || age <= 12 || age > 65) && age != 0)
+            if ((cbxDisabled.Checked || age <= 12 || age > 65) && age > 0)
             {
                 //people get a discount = true
 
@@ -39,17 +36,12 @@ namespace busfares_exercise
             }
             else
             {
-
                 discountText = " - no discount";
             }
-
 
             //display the output
             //insert the listbox at the top
             lboutput.Items.Insert(0, name + "   Ash: $" + ash + "   Dun: $" + dun + discountText);
-
         }
-
-
     }
 }
